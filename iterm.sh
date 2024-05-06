7fd0e533b6db9a2e78300ed9ed519e09f9ac6beb
johnwork <work@Johns-MacBook-Pro.local>
2024-05-06 10:24:19 +0100

    add plugins to ohmyzsh

--- iterm.sh
+++ iterm.sh
@@ -20,6 +20,26 @@ if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
 else
         echo "zsh autosuggestions not found - installing"
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+        echo "zsh syntax highlighting is installed"
+else
+        echo "zsh syntax highlighting not found - installing"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+if [ -d ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting ]; then
+        echo "fast syntax highlighting is installed"
+else
+        echo "fast syntax highlighting not found - installing"
+        git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+fi
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autocomplete ]; then
+        echo "zsh autocomplete is installed"
+else
+        echo "zsh autocomplete not found - installing"
+        git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+fi
+
         #Find a way of inserting a line into the list of plugins
         #Currently plugins=(git)
         #We want to be able to add items regardless of whether it's 
@@ -32,4 +52,4 @@ else
         #or plugins=(plugin1)
         #https://stackoverflow.com/questions/18272379/bash-inserting-a-line-in-a-file-at-a-specific-location
 
-fi
+
